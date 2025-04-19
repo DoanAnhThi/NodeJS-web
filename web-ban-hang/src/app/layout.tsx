5e59236a65a38a04bf3bd948b282932b95d5bbec
MAC <MAC@192.168.101.71.non-exists.ptr.local>
2025-04-20 02:56:32 +0700

    npm run dev success

--- web-ban-hang/src/app/layout.tsx
+++ web-ban-hang/src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Source_Code_Pro } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const codePro = Source_Code_Pro({
+  variable: "--font-code",
   subsets: ["latin"],
 });
 
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${inter.variable} ${codePro.variable}`}>
         {children}
       </body>
     </html>
